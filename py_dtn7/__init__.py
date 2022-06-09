96d15fbde76b6303e54c26d77fcf6fa29de4d2dd
Thomas Schmitt <t.e.schmitt@posteo.de>
2022-06-09 22:08:07 +0200

    :white_check_mark: adds versions test and unifies version info in pyproject and init

--- py_dtn7/__init__.py
+++ py_dtn7/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0-alpha.0"
 
 from .bundle import Bundle
 from .dtn_client import DTNClient
--- tests/test_py_dtn7.py
+++ tests/test_py_dtn7.py
@@ -1,5 +1,18 @@
-from py_dtn7 import __version__
+import toml
+from pathlib import Path
+import py_dtn7
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_versions_are_in_sync():
+    """Checks if the pyproject.toml and package.__init__.py __version__ are in sync."""
+
+    # shamelessly ripped off from this GitHub issue:
+    # https://github.com/python-poetry/poetry/issues/144#issuecomment-877835259
+    # but for real -- this problem should have been solved years ago, sheesh.
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    pyproject_version = pyproject["tool"]["poetry"]["version"]
+
+    package_init_version = py_dtn7.__version__
+
+    assert package_init_version == pyproject_version
