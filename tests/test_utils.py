35ce42ebca8b5bdc8b6e2875d96b8c057d4087b5
Thomas Schmitt <t.e.schmitt@posteo.de>
2022-06-13 22:06:37 +0200

    :white_check_mark: fun with tests

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from py_dtn7 import from_dtn_timestamp, to_dtn_timestamp
+
+
+def test_from_dtn_timestamp():
+    ts: int = 4 * 3600 * 1000 + 20 * 60 * 1000
+    correct_dt: datetime = datetime(year=2000, month=1, day=1, hour=4, minute=20, second=0)
+    assert from_dtn_timestamp(ts) == correct_dt
+
+
+def test_to_dtn_timestamp():
+    correct_ts: int = 4 * 3600 * 1000 + 20 * 60 * 1000
+    dt: datetime = datetime(year=2000, month=1, day=1, hour=4, minute=20, second=0)
+    assert to_dtn_timestamp(dt) == correct_ts
